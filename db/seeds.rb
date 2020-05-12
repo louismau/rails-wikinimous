5f66f1ac9c65d75fb78c7f6f0255592e31c58caf
Louis Mauriac <mauriaclouis@hotmail.com>
2020-05-12 16:41:11 +0200

    created a blog type app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |article|
+  article = Article.new(title: Faker::Lorem.sentence(word_count: rand(3..8)), content: Faker::Lorem.paragraph(sentence_count: rand(1..4)))
+  article.save
+end
